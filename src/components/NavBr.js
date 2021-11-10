74a2a46fd4fc5428f708cd738a484ea4332082c5
Federico Burgos <federicburgos95@gmail.com>
2021-11-10 20:55:42 -0300

    segundocommit

---
+++ src/components/NavBr.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return <div className="header">
+        <h1>Logo</h1>
+        <nav>
+            <ul>
+                <li>Enlace 1</li>
+                <li>Enlace 2</li>
+                <li>Enlace 3</li>
+            </ul>
+        </nav>
+    </div>
+}
+
+export default NavBar
