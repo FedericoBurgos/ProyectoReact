89c9877419fa8d2e42539da0a9558b0d918e4056
Federico Burgos <federicburgos95@gmail.com>
2021-11-17 22:29:39 -0300

    cuarto commit

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,17 @@
+import {Container, Row} from 'react-bootstrap'
+import {Item} from '../Item/Item'
+
+export const ItemList = ({productos}) => {
+
+    return(
+    <Container className="my-5">
+      <h2>Hola Coder!</h2>
+      <hr/>
+      <Row>
+      
+      {productos.map((prod) => <Item prod={prod}/>)}
+       </Row>
+</Container>
+  )
+}
+
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,36 @@
+import { useEffect } from "react"
 import { Container } from "react-bootstrap"
+import { ItemList } from "../ItemList/ItemList"
 
-export const ItemListContainer = ( {greeting} ) => {
-    return(
-        <Container className="my-5">
-            <h2>{greeting}</h2>
-            <hr/>
-        </Container>
-         
-    )
+export const ItemListContainer = () => {
+  const [loading,setloading] = useState(false)
+  const [productos,setProductos] = useState([])
+
+  console.log(productos)
+
+  useEffect(() => {
+      setLoading(true)
+      pedirDatos()
+          .then( (resp) => {
+              console.log(resp)
+          })
+          .catch( (error) => {
+              console.log(error)
+          })
+          .finally(() => {
+              setLoading(false)
+         })
+      
+  },[])
+
+  return (
+     <>
+  {
+      loading
+      ? <h2>Cargando...</h2>
+      : <ItemList productos={productos}/>
+      
+      }
+      </>
+)
 }
